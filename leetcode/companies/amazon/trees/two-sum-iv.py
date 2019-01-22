07d17b1a680a914278f1adb7542ec6ae332ab033
“J <jackmgrundy@gmail.com>
2019-01-22 00:05:16 -0500

    more trees

---
+++ leetcode/companies/amazon/trees/two-sum-iv.py
@@ -0,0 +1,56 @@
+"""
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+Example 1:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+Example 2:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+"""
+class TreeNode:
+    def __init__(self, num):
+        self.val = num
+        self.right = None
+        self.left = None
+
+
+class Solution:
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        queue = collections.deque()
+        queue.append(root)
+        matches = set()
+        
+        while queue:
+            curNode = queue.popleft()
+            if curNode.val in matches: return(True)
+            matches.add(k-curNode.val)
+            
+            for c in [curNode.left, curNode.right]:
+                if c: queue.append(c)
+        return(False)
+
+
+if __name__=='__main__':
+    print("a")
