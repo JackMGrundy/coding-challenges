4e835559cddbce562fd9b15ba6fb82fefd924984
“J <jackmgrundy@gmail.com>
2019-02-21 14:11:06 -0600

    design

---
+++ leetcode/companies/amazon/design/design-tic-tac-toe.py
@@ -0,0 +1,105 @@
+"""
+Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+You may assume the following rules:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves is allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Example:
+Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
+
+TicTacToe toe = new TicTacToe(3);
+
+toe.move(0, 0, 1); -> Returns 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+toe.move(0, 2, 2); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+toe.move(2, 2, 1); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+toe.move(1, 1, 2); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+toe.move(2, 0, 1); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+toe.move(1, 0, 2); -> Returns 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+Follow up:
+Could you do better than O(n2) per move() operation?
+"""
+
+# Attempt #1: 99th percentile. O(N) time per move
+class TicTacToe(object):
+
+    def __init__(self, n):
+        """
+        Initialize your data structure here.
+        :type n: int
+        """
+        self.n = n
+        self.rows = [0]*n
+        self.cols = [0]*n
+        #0 -> left to right diagonal
+        #1 -> right to left diagonal
+        self.diagonals = [0]*2
+        
+
+    def move(self, row, col, player):
+        """
+        Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins.
+        :type row: int
+        :type col: int
+        :type player: int
+        :rtype: int
+        """
+        diff = 1 if player==1 else -1
+        
+        self.rows[row] += diff
+        if abs(self.rows[row])==self.n: return(player)
+        
+        self.cols[col] += diff
+        if abs(self.cols[col])==self.n: return(player)
+        
+        #Diagonals
+        if row==col:
+            self.diagonals[0] += diff
+        if abs(row+col)==self.n-1:
+            self.diagonals[1] += diff
+        
+        if abs(self.diagonals[0])==self.n: return(player)
+        if abs(self.diagonals[1])==self.n: return(player)
+        
+        return(0)
+
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
