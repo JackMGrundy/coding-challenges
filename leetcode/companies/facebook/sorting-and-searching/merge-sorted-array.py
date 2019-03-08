8b8729b7b544c891e667ec031b722493fa093dd5
“J <jackmgrundy@gmail.com>
2019-03-07 23:40:04 -0600

    Sorting and searching

---
+++ leetcode/companies/facebook/sorting-and-searching/merge-sorted-array.py
@@ -0,0 +1,46 @@
+"""
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+"""
+# 1st attempt: 60th percentile in speed
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = len(nums1)-1
+        m -= 1; n -= 1
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -= 1
+            i -= 1
+        
+        if n >= 0:
+            nums1[:n+1] = nums2[:n+1]
+
+
+# 2nd attempt: 99th percentile in speed
+# Interesting...because the first attempt should be O(n) and this one 
+# should be nlog(n). 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
