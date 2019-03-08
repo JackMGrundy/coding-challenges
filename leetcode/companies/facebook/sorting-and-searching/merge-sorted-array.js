c2748f42a90f2ccc8351ea43987423cb957f30e7
“J <jackmgrundy@gmail.com>
2019-03-07 23:40:04 -0600

    Sorting and searching

---
+++ leetcode/companies/facebook/sorting-and-searching/merge-sorted-array.js
@@ -0,0 +1,30 @@
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+/*
+
+// 1st attempt: 75th percentile
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    for (var i=m, j=0; j < n; i++, j++) {
+        nums1[i] = nums2[j]; 
+    }
+    nums1.sort( (a, b) => a-b )
+};
