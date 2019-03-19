cddc6fe86fa1db427e3a603d8920542573bebf21
“J <jackmgrundy@gmail.com>
2019-03-19 18:08:07 -0400

    dp

--- leetcode/misc-dp/word-break.py
+++ leetcode/misc-dp/word-break.py
@@ -21,12 +21,48 @@ Example 3:
 Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 """
+# 1st attempt: Brute force BFS. Time out. Passed ~70 percent of tests.
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # s = list(s)
+        wordDict = set(wordDict)
+        
+        stack = [s]
+        while stack:
+            cur = stack.pop()
+            for i in range(len(cur)+1):
+                curSub = cur[:i]
+                # We have matched another part of the remaining string
+                if curSub in wordDict:
+                    # The match accounts for the rest of the string
+                    if i==( len(cur) ): 
+                        return(True)
+                    else:
+                        stack.append(cur[i:])
+        
+        return False
 
 
-def wordBreak(s, wordDict):
-    return 1
-
-s = "catsandog"
-words = ["cats", "dog", "sand", "and", "cat"]
-sol = wordBreak(s, words)
-print(sol)
+# 2nd attempt: 70th percentile in speed
+# A problem with the 1st attempt: we repeat a lot of work. For example, if we have a dictionary with "a" and "aa"
+# in it and a target string of "aaaa...", then we are going to confirm "aaaa" with 4 "a's" and 2 "aa's".
+# 
+# The intuition for this problem, is that no matter what, you have to match a word to the first part of the string
+# Given that you find a match, you can reduce the problem to matching the remaining, smaller string. So
+# whenever we match up to a certain point, we can mark that...it doesn't matter if there are different
+# ways of getting there. 
+# 
+# This solution simply maintains a boolean set of markers indicating how far we have succesfully matched. 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [ False ]*len(s)
+        
+        for i in range(len(s)):
+            for word in wordDict:
+                # The chars up to i match the current word...and we are either matching the first
+                # word in the string or we succesfully matched everything before the start of the
+                # current word
+                if ( word == s[i-len(word)+1:i+1] ) and ( i-len(word)==-1 or dp[i-len(word)] ):
+                    dp[i] = True
+        
+        return dp[-1]
