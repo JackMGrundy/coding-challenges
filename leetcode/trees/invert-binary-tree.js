655a8da0f128727c7dfab28098e95769769be126
“J <jackmgrundy@gmail.com>
2019-04-05 01:52:10 -0500

    trees

---
+++ leetcode/trees/invert-binary-tree.js
@@ -0,0 +1,48 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+*/
+// 1st attempt: 99th percentile in speed
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    var helper = function(node) {
+        if (node) {
+            helper(node.left);
+            helper(node.right);
+            [node.left, node.right] = [node.right, node.left];
+        }
+        return;
+    }
+    helper(root);
+    return root;
+};
---
+++ leetcode/trees/invert-binary-tree.py
@@ -0,0 +1,59 @@
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+"""
+# 1st attempt: 80th percentile in speed. Recursive.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        def helper(node):
+            if node:
+                helper(node.right)
+                helper(node.left)
+                node.left, node.right = node.right, node.left
+            return
+        helper(root)
+        return root
+
+
+# 2nd attempt: Still 80th percentile in speed.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+    
+        return root
