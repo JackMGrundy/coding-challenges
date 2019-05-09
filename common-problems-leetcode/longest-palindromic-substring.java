0229d03fec4fd1b58e05af92987ec10c2264153f
“J <jackmgrundy@gmail.com>
2019-05-09 13:55:15 -0400

    common problems

--- common-problems-leetcode/longest-palindromic-substring.java
+++ common-problems-leetcode/longest-palindromic-substring.java
@@ -15,3 +15,50 @@ Accepted
 Submissions
 1,989,622
 */
+// 50th percentile. 31ms. 
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s.length()==0) return "";
+        int maxPalindromeLen = 1;
+        int start = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            // Even length palindrome
+            if (i+1 < s.length() && s.charAt(i) == s.charAt(i+1)) {
+                int candidateMaxLen = 2;
+                int candidateStart = i;
+                int j = 1;
+                while (i-j >= 0 && i+j+1 < s.length() && s.charAt(i-j) == s.charAt(i+j+1)) {
+                    candidateStart = i-j;
+                    candidateMaxLen += 2;
+                    j++;
+                }
+                
+                if (candidateMaxLen > maxPalindromeLen) {
+                    maxPalindromeLen = candidateMaxLen;
+                    start = candidateStart;
+                }
+            }
+            
+            // Odd length palindrome
+            if (i > 0 && i+1 < s.length() && s.charAt(i-1) == s.charAt(i+1)) {
+                int candidateMaxLen = 3;
+                int candidateStart = i-1;
+                int j = 1;
+                while (i-j-1 >= 0 && i+j+1 < s.length() && s.charAt(i-j-1) == s.charAt(i+j+1)) {
+                    candidateStart = i-j-1;
+                    candidateMaxLen += 2;
+                    j++;
+                }
+                
+                if (candidateMaxLen > maxPalindromeLen) {
+                    maxPalindromeLen = candidateMaxLen;
+                    start = candidateStart;
+                }
+            }
+        
+        }
+        return s.substring(start, start+maxPalindromeLen);
+    }
+}
