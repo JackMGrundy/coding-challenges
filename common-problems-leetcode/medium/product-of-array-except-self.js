d81bc6d02be7113901d580de886a79da0c53c973
“J <jackmgrundy@gmail.com>
2019-08-05 03:04:16 -0500

    multiplying

--- common-problems-leetcode/medium/product-of-array-except-self.js
+++ common-problems-leetcode/medium/product-of-array-except-self.js
@@ -10,7 +10,7 @@ Note: Please solve it without division and in O(n).
 Follow up:
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 */
-// 1st attempt: 100th percentile
+// 1st attempt: 100th percentile. 72ms.
 /**
  * @param {number[]} nums
  * @return {number[]}
