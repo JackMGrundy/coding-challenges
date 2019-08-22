9697c2da0ccf9f15cef1add892e4a02721381ee5
“J <jackmgrundy@gmail.com>
2019-08-22 13:53:51 -0400

    trees

---
+++ companies-leetcode/bloomberg/trees-and-graphs/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,74 @@
+/*
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Example:
+
+
+
+Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+
+Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+ 
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+*/
+
+/*
+Excellent problem...for me this hammers home to not get too rooted in thinking about tree traversal as inorder, preoder,
+postorder, bst...it's natual when seeing this problem to immediately start thinking how you can tackle it with those 4.
+However, the best solution comes out of realizing that new traversal patterns are possible now. You don't have to do bfs
+or some fancy simultaneous dfs in both branches to maintain info about an entire level. As long as you link as you go down,
+you have a bridge to the other side. 
+*/
+// 0ms. 100th percentile.
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+  public Node connect(Node root) {
+      Node res = root;
+      while (root != null && root.left != null) {
+          Node cur = root;
+          while (cur != null) {
+              cur.left.next = cur.right;
+              cur.right.next = cur.next == null ? null : cur.next.left;
+              cur = cur.next;
+          }
+          root = root.left;
+      }
+      
+      return res;
+  }
+}
---
+++ companies-leetcode/bloomberg/trees-and-graphs/populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,95 @@
+"""
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Example:
+
+
+
+Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+
+Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+ 
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+"""
+# O(N) space and O(N) time. 68ms. 76th percentile.
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        
+        nodeNumber = 1
+        level = 1
+        q = deque([(root, nodeNumber, level)])
+        
+        while q:
+            cur, num, level = q.popleft()
+            
+            if num != (2**level)-1:
+                cur.next = q[0][0]
+            
+            for child in [cur.left, cur.right]:
+                nodeNumber += 1
+                if child != None:
+                    q.append( (child, nodeNumber, level+1) )
+        
+        return root
+
+
+# O(N) time. O(1) Space.
+# 64ms. 92nd percentile.
+"""
+Excellent problem...for me this hammers home to not get too rooted in thinking about tree traversal as inorder, preoder,
+postorder, bst...it's natual when seeing this problem to immediately start thinking how you can tackle it with those 4.
+However, the best solution comes out of realizing that new traversal patterns are possible now. You don't have to do bfs
+or some fancy simultaneous dfs in both branches to maintain info about an entire level. As long as you link as you go down,
+you have a bridge to the other side. 
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        res = root
+        while root and root.left:
+            cur = root
+            while cur:
+                cur.left.next = cur.right
+                cur.right.next = None if cur.next==None else cur.next.left
+                cur = cur.next
+            root = root.left
+        return res
