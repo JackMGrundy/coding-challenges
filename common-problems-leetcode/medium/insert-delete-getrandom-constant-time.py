05fb72be1662638a4a354769f68599cd11515162
“J <jackmgrundy@gmail.com>
2019-08-24 00:04:30 -0400

    common medium

--- common-problems-leetcode/medium/insert-delete-getrandom-constant-time.py
+++ common-problems-leetcode/medium/insert-delete-getrandom-constant-time.py
@@ -41,3 +41,74 @@ use hashfunction that hashes to range of 0-16...
 double whenever full...
 For random selection...select a random val in the range...check is the slot is taken...if not...try again until a hit
 """
+
+# 112ms. 94th percentile.
+# Burned through this with a very tired mind...realized after the fact a stack would be a bit better...
+# not worrying about it now...looks like its still fast at least...
+from random import choice
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.valsToIds = {}
+        self.IdsToVals = {}
+        self.nextIDToAssign = 0
+        self.smallestId = 0
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.valsToIds:
+            return False
+        
+        self.valsToIds[val] = self.nextIDToAssign
+        self.IdsToVals[self.nextIDToAssign] = val
+        self.nextIDToAssign += 1
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.valsToIds:
+            return False
+        
+        valIdToDelete = self.valsToIds[val]
+        del self.IdsToVals[valIdToDelete]
+        del self.valsToIds[val]
+        
+        if valIdToDelete == self.smallestId:
+            self.smallestId += 1
+            return True
+        else:
+            replacementVal = self.IdsToVals[self.smallestId]
+            del self.IdsToVals[self.smallestId]
+            del self.valsToIds[replacementVal]
+            self.smallestId += 1
+        
+            self.IdsToVals[valIdToDelete] = replacementVal
+            self.valsToIds[replacementVal] = valIdToDelete
+            
+
+            return True
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        if self.smallestId == self.nextIDToAssign:
+            return None
+        return self.IdsToVals[choice(range(self.smallestId, self.nextIDToAssign))]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
