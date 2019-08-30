ad9e269e6ea5166977339b49ea2aa1230b236ad4
“J <jackmgrundy@gmail.com>
2019-08-29 22:01:54 -0400

    common medium

---
+++ common-problems-leetcode/medium/find-all-duplicates-in-array.py
@@ -0,0 +1,44 @@
+"""
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3] 
+"""
+
+# 420ms. 69 percentile
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+        
+        for i,num in enumerate(nums):
+            index = abs(nums[i])-1
+            if nums[index] < 0:
+                res.append(index+1)
+            else:
+                nums[index] *= -1
+        
+        return res
+
+
+
+"""
+Notes:
+First impulse is to make a messy swap solution like the first one.
+The key thing to note though is that we just want to use an array element to hold two pieces of info
+1) have we already seen a value of that index
+2) the actual number there
+
+The swapping business is try to sort out the actual number there so that we can store "we have seen the value
+of this index".
+
+The smarter solution is to just make the element store both. The absolute value of the number there holds
+2) and whether or not it's negative holds 1). 
+"""
