40d903a4833d18353d9c9282a7b39e2c88fb4a74
“J <jackmgrundy@gmail.com>
2019-09-07 00:10:36 -0400

    notes

--- common-problems-leetcode/medium/subarray-sum-equals-k.py
+++ common-problems-leetcode/medium/subarray-sum-equals-k.py
@@ -49,3 +49,25 @@ class Solution:
             matches[sm+k] += 1
         
         return res
+
+
+"""
+Notes:
+
+Say you have an arr and you have an array with cumulative sums for the array. Now let's say there are two 
+cumulative sums that are the same. That means the sum of the values between them must be 0.
+
+Extending this, say the different between the two is k, then the array between them must be k. 
+
+So for every cumulative sum, we make that sum+k in a hashtable. Now we know that if we later see
+such a sum, we've found an array with sum k.
+
+Final bits
+
+1) We don't just mark that we've seen this cumulative sum. Rather we count how many such
+cumulative sums we've seen. 
+
+2) We need to mark matches[k] equal to 1 to start. To see why we need this, imagine we didn't have it. We 
+might march through an array with a sum equal to k, and we wouldn't recognize it as a match. 
+
+"""
