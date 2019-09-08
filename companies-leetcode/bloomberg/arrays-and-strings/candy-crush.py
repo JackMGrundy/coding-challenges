3d74640bbb8e90c6c74e1104004ca92c3e9b5cb7
“J <jackmgrundy@gmail.com>
2019-09-08 15:53:36 -0400

    edits

--- companies-leetcode/bloomberg/arrays-and-strings/candy-crush.py
+++ companies-leetcode/bloomberg/arrays-and-strings/candy-crush.py
@@ -29,219 +29,48 @@ Note:
 The length of board will be in the range [3, 50].
 The length of board[i] will be in the range [3, 50].
 Each board[i][j] will initially start as an integer in the range [1, 2000].
-"""
-# 380ms. 12th percentile.
-import copy
+"""        
+# 224ms. 51 percentile.
 class Solution:
-    streakThreshold = 3
-    
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        if not board:
+            return board
+        
         crushed = True
         while crushed:
-            crushed, board = self.crush(board)
-            board = self.drop(board)
-        
-        return board
-    
-
-    def crush(self, board):
-        squaresToCrush = [ [ False for square in row ] for row in board ]
-        originalBoard = copy.deepcopy(board)
-        
-        # rows
-        for y, row in enumerate(board):
-            streakCount = 1
-            previousValue = None
-            for x, currentValue in enumerate(row):
-                if currentValue != 0 and previousValue == currentValue and streakCount >= self.streakThreshold:
-                    squaresToCrush[y][x] = True
-                elif currentValue != 0 and previousValue == currentValue and streakCount < self.streakThreshold:
-                    streakCount += 1
-                    if streakCount >= self.streakThreshold:
-                        for i in range(x-streakCount+1, x+1):
-                            squaresToCrush[y][i] = True    
-                else:
-                    streakCount = 1
-                    previousValue = currentValue
-        
-        # columns
-        for x in range(len(board[0])):
-            streakCount = 1
-            previousValue = None
-            for y in range(len(board)):
-                currentValue = board[y][x]
-                if currentValue != 0 and previousValue == currentValue and streakCount >= self.streakThreshold:
-                    squaresToCrush[y][x] = True
-                elif currentValue != 0 and previousValue == currentValue and streakCount < self.streakThreshold:
-                    streakCount += 1
-                    if streakCount >= self.streakThreshold:
-                        for i in range(y-streakCount+1, y+1):
-                            squaresToCrush[i][x] = True  
-                else:
-                    streakCount = 1
-                    previousValue = currentValue
-        
-        for y in range(len(board)):
-            for x in range(len(board[0])):
-                if squaresToCrush[y][x]:
-                    board[y][x] = 0
-        
-        return (True, board) if originalBoard != board else (False, board)
-    
-            
-    
-    def drop(self, board):
-        
-        for x in range(len(board[0])):
-            tail = runner = len(board)-1
-            while runner >= 0 and tail >= 0:
-                while tail >= 0 and board[tail][x] != 0:
-                    tail -= 1
-                runner = tail
-                while runner >= 0 and board[runner][x] == 0:              
-                    runner -= 1
-                
-                while runner >= 0 and tail >= 0 and board[tail][x] == 0 and board[runner][x] != 0:
-                    board[tail][x], board[runner][x] = board[runner][x], board[tail][x]
-                    tail -= 1
-                    runner -= 1
-        
-        return board
-                
-                
-                
-            
-            
-# 208ms 61st percentile 
-class Solution:
-    streakThreshold = 3
-    
-    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        crushed = True
-        while crushed:
-            crushed, board = self.crush(board)
+            crushed = self.crush(board)
             if crushed:
-                board = self.drop(board)
+                self.drop(board)
         
-        return board
-    
-    
-    def crush(self, board):
-        squaresToCrush = [ [ False for square in row ] for row in board ]
-        crushed = False
-        
-        # rows
-        for y, row in enumerate(board):
-            streakCount = 1
-            previousValue = None
-            for x, currentValue in enumerate(row):
-                if board[y][x]==0 or previousValue != currentValue:
-                    streakCount = 1
-                    previousValue = currentValue
-                elif streakCount >= self.streakThreshold:
-                    squaresToCrush[y][x] = True
-                elif streakCount < self.streakThreshold:
-                    streakCount += 1
-                    if streakCount >= self.streakThreshold:
-                        for i in range(x-streakCount+1, x+1):
-                            squaresToCrush[y][i] = True    
-                            crushed = True
-
-        # columns
-        for x in range(len(board[0])):
-            streakCount = 1
-            previousValue = None
-            for y in range(len(board)):
-                currentValue = board[y][x]
-                if board[y][x]==0 or previousValue != currentValue:
-                    streakCount = 1
-                    previousValue = currentValue
-                elif streakCount >= self.streakThreshold:
-                    squaresToCrush[y][x] = True
-                elif streakCount < self.streakThreshold:
-                    streakCount += 1
-                    if streakCount >= self.streakThreshold:
-                        for i in range(y-streakCount+1, y+1):
-                            squaresToCrush[i][x] = True 
-                            crushed = True
-        
-        for y in range(len(board)):
-            for x in range(len(board[0])):
-                if squaresToCrush[y][x]:
-                    board[y][x] = 0
-        
-        return (crushed, board)
-    
-            
-    
-    def drop(self, board):
-        
-        for x in range(len(board[0])):
-            tail = runner = len(board)-1
-            while runner >= 0 and tail >= 0:
-                while tail >= 0 and board[tail][x] != 0:
-                    tail -= 1
-                runner = tail
-                while runner >= 0 and board[runner][x] == 0:              
-                    runner -= 1
-                
-                while runner >= 0 and tail >= 0 and board[tail][x] == 0 and board[runner][x] != 0:
-                    board[tail][x], board[runner][x] = board[runner][x], board[tail][x]
-                    tail -= 1
-                    runner -= 1
-        
-        return board
-                
-
-
-
-# 208ms. 61st percentile
-# cleaner
-class Solution:
-    streakThreshold = 3
-    
-    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        crushed = True
-        while crushed:
-            crushed, board = self.crush(board)
-            if crushed:
-                board = self.drop(board)
         return board
     
     def crush(self, board):
-        squaresToCrush = [ [ 0 for square in row ] for row in board ]
         crushed = False
+        crushedCells = set([])
         
-        # rows
-        for y, row in enumerate(board):
-            for x, currentValue in enumerate(row[:len(row)-2]):
-                if board[y][x] != 0 and board[y][x] == board[y][x+1] == board[y][x+2]:
-                    squaresToCrush[y][x], squaresToCrush[y][x+1], squaresToCrush[y][x+2] = 1, 1, 1
+        for j in range(len(board)):
+            for i in range(len(board[0])):
+                # crush column
+                if board[j][i] != 0 and j+2 < len(board) and board[j][i] == board[j+1][i] == board[j+2][i]:
+                    crushedCells.update( [(j,i), (j+1,i), (j+2,i)] )
+                    crushed = True
+                
+                # crush row
+                if board[j][i] != 0 and i+2 < len(board[0]) and board[j][i] == board[j][i+1] == board[j][i+2]:
+                    crushedCells.update( [(j,i), (j,i+1), (j,i+2)] )
                     crushed = True
         
-        # columns
-        for x in range(len(board[0])):
-            for y in range(len(board[:len(board)-2])):
-                if board[y][x] != 0 and board[y][x] == board[y+1][x] == board[y+2][x]:
-                    squaresToCrush[y][x], squaresToCrush[y+1][x], squaresToCrush[y+2][x] = 1, 1, 1
-                    crushed = True
+        for cell in crushedCells:
+            j,i = cell
+            board[j][i] = 0
         
-        for y in range(len(board)):
-            for x in range(len(board[0])):
-                if squaresToCrush[y][x] == 1:
-                    board[y][x] = 0
-        
-        return (crushed, board)
-
+        return crushed
     
+    # Similar to partition in quicksort
     def drop(self, board):
-        for x in range(len(board[0])):
-            tail = len(board)-1
-            for runner in range(len(board)-1, -1, -1):
-                if board[runner][x] > 0:
-                    board[tail][x] = board[runner][x]
-                    tail -= 1
-            for i in range(tail, -1, -1):
-                board[i][x] = 0
-
-        return board
+        for i in range(len(board[0])):
+            swapIndex = len(board)-1
+            for j in range(len(board)-1, -1, -1):
+                if board[j][i] != 0:
+                    board[j][i], board[swapIndex][i] = board[swapIndex][i], board[j][i]
+                    swapIndex -= 1
