23da7d2af334776869003940e8d08493fd87853f
“J <jackmgrundy@gmail.com>
2019-09-09 15:07:39 -0400

    edits

--- backtracking-leetcode/word-search.py
+++ backtracking-leetcode/word-search.py
@@ -16,222 +16,33 @@ Given word = "ABCCED", return true.
 Given word = "SEE", return true.
 Given word = "ABCB", return false.
 """
-# Attempt 1: passes 85/87 test case and then timeouts on the big ones
+
+# Cleanest. 356ms. 75 percentile.
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        res = False
-        
-        def inRange(y, x, j, i):
-            return ( j >= 0 and j < y and i >= 0 and i < x )
-        
-        def validNeighbors(y, x, j, i):
-            neighbors = [ (j+1, i), (j-1, i), (j, i+1), (j, i-1) ]
-            res = []
-            for neighbor in neighbors:
-                jx, ix = neighbor
-                if inRange(y, x, jx, ix):
-                    res.append(neighbor)
-            return res
-        
-        
-        def search(jc, ic, cur):
-            nonlocal res
-            # Matched all the letters in the word
-            if cur==len(word):
-                res = True
-            # Keep matching
-            else:
-                # Check all valid neighbors
-                neighbors = validNeighbors(m, n, jc, ic)
-                for neighbor in neighbors:
-                    jn, ji = neighbor
-                    # If the neighbor is a match and not already visited, recurse
-                    if board[jn][ji]==word[cur] and (jn, ji) not in visited:
-                        visited.add( (jn, ji) )
-                        search(jn, ji, cur+1)
-                # Ended recursion. Back track and remove cur from visited
-                visited.remove( (jc, ic) )
-
-        
-        # Iterate through squares on board
-        for i in range(n):
-            for j in range(m):
-                visited = set()
+        def dfs(i, j, curCharIndex):
+            if curCharIndex == len(word):
+                return True
+            
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == word[curCharIndex]:
+                temp = board[i][j]
+                board[i][j] = "*"
                 
-                if word[0]==board[j][i]:
-                    visited.add( (j, i) )
-                    search(j, i, 1)
-        
-        return res
-
-# 2nd attempt: 16th percentile in speed. Cut off unneccessary search.
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        res = False
-        
-        def inRange(y, x, j, i):
-            return ( j >= 0 and j < y and i >= 0 and i < x )
-        
-        def validNeighbors(y, x, j, i):
-            neighbors = [ (j+1, i), (j-1, i), (j, i+1), (j, i-1) ]
-            res = []
-            for neighbor in neighbors:
-                jx, ix = neighbor
-                if inRange(y, x, jx, ix):
-                    res.append(neighbor)
-            return res
-        
-        
-        def search(jc, ic, cur):
-            nonlocal res
-            if res: return
-            # Matched all the letters in the word
-            if cur==len(word):
-                res = True
-            # Keep matching
-            else:
-                # Check all valid neighbors
-                neighbors = validNeighbors(m, n, jc, ic)
-                for neighbor in neighbors:
-                    jn, ji = neighbor
-                    # If the neighbor is a match and not already visited, recurse
-                    if board[jn][ji]==word[cur] and (jn, ji) not in visited:
-                        visited.add( (jn, ji) )
-                        search(jn, ji, cur+1)
-                # Ended recursion. Back track and remove cur from visited
-                visited.remove( (jc, ic) )
-
-        
-        # Iterate through squares on board
-        for i in range(n):
-            for j in range(m):
-                visited = set()
+                matched = dfs(i+1, j, curCharIndex+1) or dfs(i-1, j, curCharIndex+1) or dfs(i, j+1, curCharIndex+1) or dfs(i, j-1, curCharIndex+1)
                 
-                if word[0]==board[j][i]:
-                    visited.add( (j, i) )
-                    search(j, i, 1)
-        
-        return res
-
-
-# 3rd attempt: 16th percentile in speed. No diff between nonlocal and class variable
-class Solution:
-    res = False
-    
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        
-        def inRange(y, x, j, i):
-            return ( j >= 0 and j < y and i >= 0 and i < x )
-        
-        def validNeighbors(y, x, j, i):
-            neighbors = [ (j+1, i), (j-1, i), (j, i+1), (j, i-1) ]
-            valid = []
-            for neighbor in neighbors:
-                jx, ix = neighbor
-                if inRange(y, x, jx, ix):
-                    valid.append(neighbor)
-            return valid
-        
-        
-        def search(jc, ic, cur):
-            if self.res: return
-            # Matched all the letters in the word
-            if cur==len(word):
-                self.res = True
-            # Keep matching
+                board[i][j] = temp
+                return matched
             else:
-                # Check all valid neighbors
-                neighbors = validNeighbors(m, n, jc, ic)
-                for neighbor in neighbors:
-                    jn, ji = neighbor
-                    # If the neighbor is a match and not already visited, recurse
-                    if board[jn][ji]==word[cur] and (jn, ji) not in visited:
-                        visited.add( (jn, ji) )
-                        search(jn, ji, cur+1)
-                # Ended recursion. Back track and remove cur from visited
-                visited.remove( (jc, ic) )
+                return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0] and dfs(i, j, 0):
+                    return True
+        
+        return False
 
-        
-        # Iterate through squares on board
-        for i in range(n):
-            for j in range(m):
-                visited = set()
-                
-                if word[0]==board[j][i]:
-                    visited.add( (j, i) )
-                    search(j, i, 1)
-        
-        return self.res
-
-# Attempt 4: 63rd percentile. Cleaning up the neighbor selection sped things up
-class Solution:
-    res = False
-    
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        
-        def validNeighbors(y, x, j, i):
-            valid = []
-            if i > 0:
-                valid.append( (j, i-1) )
-            if i < x-1:
-                valid.append( (j, i+1) )
-            if j > 0:
-                valid.append( (j-1, i) )
-            if j < y-1:
-                valid.append( (j+1, i) )
-            return valid
-        
-        
-        def search(jc, ic, cur):
-            if self.res: return
-            # Matched all the letters in the word
-            if cur==len(word):
-                self.res = True
-            # Keep matching
-            else:
-                # Check all valid neighbors
-                neighbors = validNeighbors(m, n, jc, ic)
-                for neighbor in neighbors:
-                    jn, ji = neighbor
-                    # If the neighbor is a match and not already visited, recurse
-                    if board[jn][ji]==word[cur] and (jn, ji) not in visited:
-                        visited.add( (jn, ji) )
-                        search(jn, ji, cur+1)
-                # Ended recursion. Back track and remove cur from visited
-                visited.remove( (jc, ic) )
-
-        
-        # Iterate through squares on board
-        for i in range(n):
-            for j in range(m):
-                visited = set()
-                
-                if word[0]==board[j][i]:
-                    visited.add( (j, i) )
-                    search(j, i, 1)
-        
-        return self.res
-
-# Attempt 5: 95th percentile in speed. Cleaner
-"""
-Intuition:
-There are some cool idea to take away from this. You can think of each recursive call
-as an additional condition that must be true in order for the whole process to be true.
-...the first recursive dfs call can be thought of as a long string of additional if statements. 
-
-Also note the backtracking nature of this. In the first call, say the first if statement is evaluated to 
-True...then we are going to do a dfs search down this line of thought until we miss. If we do, we'll
-switch the square back to not-visited (i.e. not "#"), and then back up until we're at a call where
-we can trigger another true dfs call. 
-"""
+# Fastest. 232ms. 98th percentile.
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m = len(board)
@@ -261,4 +72,17 @@ class Solution:
                 if board[j][i]==word[0] and dfs(board, j, i, word[1:]):
                     return True
         return False
-       
+
+"""
+Notes:
+
+Intuition:
+There are some cool idea to take away from this. You can think of each recursive call
+as an additional condition that must be true in order for the whole process to be true.
+...the first recursive dfs call can be thought of as a long string of additional if statements. 
+
+Also note the backtracking nature of this. In the first call, say the first if statement is evaluated to 
+True...then we are going to do a dfs search down this line of thought until we miss. If we do, we'll
+switch the square back to not-visited (i.e. not "#"), and then back up until we're at a call where
+we can trigger another true dfs call. 
+"""
