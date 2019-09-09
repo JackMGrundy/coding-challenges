064220d8ecaa409b88296acd05cd1f29d1ae0c93
“J <jackmgrundy@gmail.com>
2019-09-09 15:52:23 -0400

    edits

--- companies-leetcode/bloomberg/linked-lists/add-two-numbers-ii.py
+++ companies-leetcode/bloomberg/linked-lists/add-two-numbers-ii.py
@@ -11,35 +11,52 @@ Example:
 Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 8 -> 0 -> 7
 """
-# 72ms. 97th percentile.
+# 64ms. 99.8 percentile.
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+"""
+d -> 9 -> 9 -> 9
+d -> 1 -> 1 -> 1
+   
+1 -> 1 -> 0 -> 0
+
+
+"""
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
-        l1Num = 0
+        l1Value, l2Value = 0, 0
         while l1:
-            l1Num = l1Num*10 + l1.val
+            l1Value = l1Value*10 + l1.val
             l1 = l1.next
-        
-        l2Num = 0
         while l2:
-            l2Num = l2Num*10 + l2.val
+            l2Value = l2Value*10 + l2.val
             l2 = l2.next
+        total = l1Value + l2Value
+        if total == 0:
+            return ListNode(0)
         
-        l1Plusl2 = l1Num + l2Num
-        head = ListNode( l1Plusl2 % 10 )
-        l1Plusl2 = l1Plusl2 // 10
-        
-        while l1Plusl2:
-            temp = ListNode( l1Plusl2 % 10 )
-            l1Plusl2 = l1Plusl2 // 10
+        head = None
+        while total:
+            nextDigit = total % 10
+            total = total // 10
+            temp = ListNode(nextDigit)
             temp.next = head
             head = temp
         
         return head
-            
+
+"""
+Notes:
+
+Simply find the two numbers the lists represent them, add them, construct the output list one digit at a time.
+"""
