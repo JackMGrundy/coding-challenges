533ceed1e743ed2430cfc7f1e808f37bfe7a951d
“J <jackmgrundy@gmail.com>
2019-09-08 22:39:53 -0400

    notes

--- common-problems-leetcode/medium/longest-substring-without-repeating-characters.py
+++ common-problems-leetcode/medium/longest-substring-without-repeating-characters.py
@@ -22,27 +22,35 @@ Accepted
 Submissions
 3,144,652
 """
-# 94th percentile
+# 93 percentile
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s: return 0
-        seen = set()
-        curBest = tail = res = 0
+        if not s:
+            return 0
+        longestLength = 1
+        characterToIndex = {}
+        left = 0
         
-        for head, c in enumerate(s):
-            # Not repeating char
-            if c not in seen:
-                curBest += 1
-            # Repeating char
-            else:
-                # Check if we found a new best answer
-                res = max(res, curBest)
-                while s[tail]!=c:
-                    seen.remove(s[tail])
-                    tail += 1
-                tail += 1
-                curBest = head-tail+1
-            
-            seen.add(c)
+        for right, c in enumerate(s):
+            if c in characterToIndex and left <= characterToIndex[c]:
+                longestLength = max(longestLength, right - left)
+                left = characterToIndex[c] + 1
+            characterToIndex[c] = right
         
-        return max(res, curBest)
+        return max(longestLength, len(s) - left)
+
+"""
+Notes:
+You could use a set to check for repeats and then advance the left marker until you don't have repeats. To speed things up,
+We can use a hash table instead and instantly advance the left marker to the first valid point. 
+
+The one important note is the "and left <= characterToIndex[c]:"
+
+This says "the last occurence of this repeat character has to be before or equal to left"
+This precludes cases like "tmmzuxt" where the repeat offender is t but left does not equal the last occurence
+of t because we already advanced to deal with the repeat m's.
+In this case, resetting left would reintroduce the duplicate m's to the string, because it would set left to
+"the last occurrency of t+1".
+
+In this case we want to leave left where it is, because it is already farther than the last occurence. 
+"""
