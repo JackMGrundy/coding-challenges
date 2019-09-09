9107f9543bf35ba3770b8775b3dc18f41ab6950f
“J <jackmgrundy@gmail.com>
2019-09-09 16:52:13 -0400

    edits

--- dp-leetcode/word-break.py
+++ dp-leetcode/word-break.py
@@ -21,48 +21,25 @@ Example 3:
 Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 """
-# 1st attempt: Brute force BFS. Time out. Passed ~70 percent of tests.
+# 36ms. 98th percentile.
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # s = list(s)
-        wordDict = set(wordDict)
+        dp = [False]*len(s)
         
-        stack = [s]
-        while stack:
-            cur = stack.pop()
-            for i in range(len(cur)+1):
-                curSub = cur[:i]
-                # We have matched another part of the remaining string
-                if curSub in wordDict:
-                    # The match accounts for the rest of the string
-                    if i==( len(cur) ): 
-                        return(True)
+        for i in range(len(dp)):
+            for word in wordDict:
+                if i + len(word) <= len(s) and s[i:i+len(word)] == word and (i == 0 or dp[i] == True):
+                    if i + len(word) == len(s):
+                        return True
                     else:
-                        stack.append(cur[i:])
-        
+                        dp[i + len(word)] = True
         return False
 
+"""
+Notes:
+Starting at the beginning of the string, check every word that can match some starting portion of the string
+and mark the point "matched to" in the dp array.
 
-# 2nd attempt: 70th percentile in speed
-# A problem with the 1st attempt: we repeat a lot of work. For example, if we have a dictionary with "a" and "aa"
-# in it and a target string of "aaaa...", then we are going to confirm "aaaa" with 4 "a's" and 2 "aa's".
-# 
-# The intuition for this problem, is that no matter what, you have to match a word to the first part of the string
-# Given that you find a match, you can reduce the problem to matching the remaining, smaller string. So
-# whenever we match up to a certain point, we can mark that...it doesn't matter if there are different
-# ways of getting there. 
-# 
-# This solution simply maintains a boolean set of markers indicating how far we have succesfully matched. 
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [ False ]*len(s)
-        
-        for i in range(len(s)):
-            for word in wordDict:
-                # The chars up to i match the current word...and we are either matching the first
-                # word in the string or we succesfully matched everything before the start of the
-                # current word
-                if ( word == s[i-len(word)+1:i+1] ) and ( i-len(word)==-1 or dp[i-len(word)] ):
-                    dp[i] = True
-        
-        return dp[-1]
+Iterate through the cars of the string. At each char, if we have matched this far, try to match farther into the string.
+If we match to the last character, retun true. 
+"""
