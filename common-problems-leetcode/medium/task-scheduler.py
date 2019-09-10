1846febe96fc0d6772c63f1b98c89ab1dd5fe7a3
“J <jackmgrundy@gmail.com>
2019-09-10 16:43:49 -0400

    edits and notes

--- common-problems-leetcode/medium/task-scheduler.py
+++ common-problems-leetcode/medium/task-scheduler.py
@@ -23,36 +23,72 @@ Accepted
 Submissions
 181,812
 """
-# 43rd percentile. 228ms
+# 37rd percentile. 636ms
 # programmatically
-from collections import Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counts = Counter(tasks)
-        res = 0
-        while counts:
-            mosts = counts.most_common()
-            padding = max(0, n-len(mosts)+1)
-            
-            for i in range(0, min(len(mosts), n+1)):
-                key, val = mosts[i]
-                counts[key] = val-1
-                if counts[key]==0:
-                    del counts[key]
-                res += 1
-            
-            if counts:
-                res += padding
+        taskCounts = collections.Counter(tasks)
+        cpuIntervals = 0
         
-        return res
+        while taskCounts:
+            tasksMostCommonToLeast = taskCounts.most_common()
+            numTasksLeftInCycle = n+1
+            
+            for task in tasksMostCommonToLeast:
+                taskName, taskCount = task
+                taskCounts[taskName] -= 1
+                if taskCounts[taskName] == 0:
+                    del taskCounts[taskName]
+                numTasksLeftInCycle -= 1
+                cpuIntervals += 1
+                if numTasksLeftInCycle == 0:
+                    break
+                
+            if taskCounts:
+                cpuIntervals += numTasksLeftInCycle
+        
+        return cpuIntervals
 
-# 95th percentile. 56ms
-# math...
-from collections import Counter
+
+# 95th percentile. 424ms
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counts = Counter(tasks)
-        biggest = counts.most_common(1)[0][1]
-        countBiggest =list(counts.values()).count(biggest)
-        res = max( (biggest-1)*(n+1)+countBiggest, len(tasks))
-        return res
+        frequencies = collections.Counter(tasks)
+        maxFrequency = frequencies.most_common(1)[0][1]
+        numTasksWithMaxFrequency = sum([ 1 for task in frequencies if frequencies[task]==maxFrequency ])
+        cycleLength = n + 1
+        
+        return max(cycleLength*(maxFrequency - 1) + numTasksWithMaxFrequency, len(tasks))
+ 
+"""
+Notes:
+
+Easiest way to think about it:
+We don't really care about the names of the tasks, just how many there are. 
+If the cool down period is 1, That means for each cycle, we'll ideally have 2 different elements...so we can do something like A B A D B...
+just making sure we don't have repeats. 
+
+When we have a greater variety of elements it's easier to accomplish this. As a result, scarcer elements are relatively more valuable than
+more common ones.
+
+In each cycle, we want to use up the most common elements first. 
+
+One way to do this, is to sort the elements by frequency at the beginning of each cycle we are constructing. 
+
+
+
+Smarter way to think about it:
+A full cycle is n+1 (say n == 1, we need 1+1 = 2 elements to refresh).
+
+We can split all scenarios into two buckets:
+1) We have enough variety in tasks that the cooldown period is never an issue. In this case, the answer is simply the total number of tasks
+2) We have tasks with such high frequencies, that after making as many full tasks as we can, we still have some of them left over. In this
+case, the limiting factor isn't the total number of tasks, rather its the highest frequency tasks. We'll need at least 
+cycle-length * frequency of most common task. 
+There's only one last thing to consider here. Say we only have one task with the max occurrence. The total number of tasks+cooldown periods completed
+isn't actually cycle-length * frequency...the last "cycle" will look like A coolDown coolDown...but in this case the two coolDown periods don't
+count because we're done. Further, say we had two tasks that had the max number of occurrences, then the last cycle would look like A B cooldown (where
+we again don't count that last cooldown). 
+
+As a result, the full numbers of tasks completed is actually (cycle-length * (maxFrequency-1)) + counts of elements with max frequency
+"""
