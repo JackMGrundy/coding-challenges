02a6380770d983c1c11ed01fef6c9a54abbe85eb
“J <jackmgrundy@gmail.com>
2019-09-10 22:59:23 -0400

    Edits

--- common-problems-leetcode/medium/binary-tree-zigzag-level-order-travesal.py
+++ common-problems-leetcode/medium/binary-tree-zigzag-level-order-travesal.py
@@ -16,7 +16,7 @@ return its zigzag level order traversal as:
   [15,7]
 ]
 """
-# 1st attempt: 75th percentile in speed
+# 38ms. 90 percentile. 
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -24,66 +24,32 @@ return its zigzag level order traversal as:
 #         self.left = None
 #         self.right = None
 
-from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        if not root: return []
-        curLevel = 1
-        curList = [ ]
-        res = [ ]
-        q = deque([ (root, 1) ])
+        if not root:
+            return root
+        
+        queue = collections.deque([(root, 0)])
+        level = 0
         order = 1
-        
-        while q:
-            cur,d = q.popleft()
-            if d > curLevel:
-                curList = curList[::order]
-                res.append(curList)
-                curList = []
-                curLevel += 1
-                order *= -1
-            if cur:
-                curList.append(cur.val)
-                for c in [ cur.left, cur.right ]:
-                    q.append( (c, d+1) )
-        
-        return res
-
-# 2nd attempt: 99th percentile in speed (I think this is just due to inconsistent timing...very small absolute difference)
-"""
-Not much faster, but cleaner and interesting phrasing...
-"""
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-from collections import deque
-class Solution:
-    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        curLevelValues = []
         res = []
-        if not root: return res
         
-        order = 1
-        s = deque([root])
+        while queue:
+            cur, curLevel = queue.popleft()
+            if level < curLevel:
+                level += 1
+                res.append(curLevelValues[::order])
+                curLevelValues = []
+                order *= -1
+            
+            curLevelValues.append(cur.val)
+            
+            for c in [cur.left, cur.right]:
+                if c:
+                    queue.append( (c, curLevel + 1) )
         
-        while s:
-            level = []
-            
-            for _ in range(len(s)):
-                cur = s.popleft()
-                
-                level.append(cur.val)
-                
-                if cur.left: s.append(cur.left)
-                if cur.right: s.append(cur.right)
-            
-            if level:
-                res.append(level[::order])
-            
-            order *= -1
+        res.append(curLevelValues[::order])
         
         return res
         
--- companies-leetcode/facebook/sorting-and-searching/search-in-rotated-sorted-array.py
+++ companies-leetcode/facebook/sorting-and-searching/search-in-rotated-sorted-array.py
@@ -18,49 +18,44 @@ Example 2:
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 """
-# 1st attempt: 63rd percentile in speed
-
-# The key idea: With normal binary search, we can assume that the entire array is sorted.
-# We can't assume that here. However, no matter where the pivot is, if we pick a middle point
-# between the left and right hand sides (l and r), we can guarantee that l to m or m to r is a 
-# "normal", sorted section. Therefore, we can check if target is in that section. If it is, we
-# narrow our search to it. If not, it must in the other side. 
+# 44ms. 94th percentile. 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if not nums: return -1
-        l, r = 0, len(nums)-1
+        if not nums:
+            return -1
+        return self.rotatedBinarySearch(nums, target)
+    
+    def rotatedBinarySearch(self, nums, target):
+        left, right = 0, len(nums)-1
         
-        while l<=r:
-            m = (l+r)//2
-            if nums[m]==target:
-                return(m)
-            # Is the left hand side a "normal" ordered section
-            if nums[l] <= nums[m]:
-                # Is target in this "normal section"
-                if nums[l] <= target <= nums[m]:
-                    # Restrict the search to this section
-                    r = m-1
-                # The target is not in this section. Switch to searching the other section
+        while left <= right:
+            m = left + (right - left)//2
+            
+            if nums[m] == target:
+                return m
+            
+            # Left hand side is sorted normally
+            if nums[0] <= nums[m]:
+                if nums[0] <= target <= nums[m]:
+                    right = m - 1
                 else:
-                    l = m+1
-            # The right hand side is a "normal" ordered section
+                    left = m + 1
+            # Right hand side is sorted normally
             else:
-                # Is target in this "normal section"
-                if nums[m] <= target <= nums[r]:
-                    # Restrict the search to this section
-                    l = m+1
-                # The target is not in this section. Switch to searching the other section
+                if nums[m] <= target <= nums[-1]:
+                    left = m + 1
                 else:
-                    r = m-1
+                    right = m - 1
         
         return -1
-                
-        
-# Second attempt: 
-# 1st attempt was 44ms. 99th percentile is 40ms. I looked at a few examples
-# that were that fast, and they used the same core idea but had more confusing
-# code in my opinion. 
 
 
+"""
+Notes:
+The key idea: With normal binary search, we can assume that the entire array is sorted.
+We can't assume that here. However, no matter where the pivot is, if we pick a middle point
+between the left and right hand sides (l and r), we can guarantee that l to m or m to r is a 
+"normal", sorted section. Therefore, we can check if target is in that section. If it is, we
+narrow our search to it. If not, it must in the other side. 
 
-
+"""
