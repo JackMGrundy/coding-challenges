689e6792eb4d94e2b7e14353acb9838856b61028
“J <jackmgrundy@gmail.com>
2019-09-10 22:41:36 -0400

    edits

--- common-problems-leetcode/medium/product-of-array-except-self.py
+++ common-problems-leetcode/medium/product-of-array-except-self.py
@@ -10,36 +10,32 @@ Note: Please solve it without division and in O(n).
 Follow up:
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 """
-# 1st attempt: 42nd percentile in speed. O(N) space
+# 140ms. 65 percentile.
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        back = nums[:]
+        output = [ x for x in nums ]
         for i in range(1, len(nums)):
-            nums[i] *= nums[i-1]
-        for i in range(len(nums)-2, -1, -1):
-            back[i] *= back[i+1]
-        res = [0]*len(nums)
-        res[0] = back[1]
-        res[-1] = nums[-2]
-        for i in range(1, len(nums)-1):
-            res[i] = nums[i-1] * back[i+1]
-        return res
-
-# 2nd attempt: 65th percentile in speed. O(1) space.
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = nums[:]
-        for i in range(len(nums)-2, -1, -1):
-            nums[i] *= nums[i+1]
+            output[i] *= output[i-1]
         
-        temp = temp2 = res[0]
-        res[0] = nums[1]
-        for i in range(1, len(nums)-1):
-            temp2 = res[i]
-            res[i] = temp * nums[i+1]
-            temp *= temp2
-        res[-1] = temp
+        output = output
+        rollingTotal = 1
+        for i in range(len(output)-1, 0, -1):
+            output[i] = output[i-1]*rollingTotal
+            rollingTotal *= nums[i]
+        output[0] = rollingTotal
         
-        return res
+        return output
 
-# ...too sleepy to keep going rn
+"""
+Notes:
+
+Consider the example:
+[1,2,3,4]
+
+We initialize the output array to be a rolling product of these numbers:
+[1 2 6 24]
+
+Then we iterate backwards through this array and maintain a rolling product constant comprised of elements from the original array.
+For each element, we have the product of the all the elements before it contained in the preceding element. And we have the product
+of all the elements after it in the array contained in our constat. Ojala, everything we need to fill each element
+"""
