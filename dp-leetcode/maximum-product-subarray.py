6b3978fc5a549737a25e8468423ef81ffcdbc3f8
“J <jackmgrundy@gmail.com>
2019-09-12 19:38:43 -0400

    common medium

--- dp-leetcode/maximum-product-subarray.py
+++ dp-leetcode/maximum-product-subarray.py
@@ -12,13 +12,15 @@ Input: [-2,0,-1]
 Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 """
-# 75th percentile in speed. 
+# 60ms. 92 percentile. O(1) space
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if not nums: return None
-        if len(nums)==1: return nums[0]
+        if not nums: 
+            return None
+        if len(nums)==1: 
+            return nums[0]
         
-        curMin = curMax = absMin = absMax = nums[0]
+        curMin = curMax = absMax = nums[0]
 
         for num in nums[1:]:
             
@@ -27,29 +29,13 @@ class Solution:
             curMin = min( num, num*curMin)
             curMax = max( num, num*curMax)
             
-            absMin = min( curMin, absMin )
             absMax = max( curMax, absMax )
             
         return absMax
 
 
 
-# 95th percentile in speed. Favorite answer
-
-# Intuition: say you have a string of numbers with a single negative in the middle. You are going to achieve
-# a max sum by multiplying up to that negative on either the left side or the right side.
-# Now say there are two negatives. Assuming there are positives on either side of the array, you cannot achieve a 
-# max between the negatives (it will be less than 0). However, assuming you multiply through both of them, it's as 
-# though they are positive. Therefore, the max will be achieved by multiplying all of the numbers.
-# Say you have three negatives. This is a combination of the first two scenarios. Using all 3 in a product sticks you with
-# a negative number. However, taking a product the involes the first two or the last two will yield the largest number.
-# In all three of these series, if you multiply through from both the left and right side, you're guaranteed to hit
-# the max product along the way.
-
-# This implication holds for 4 negatives, 5, and so (you can make an inductive argument). So it always holds. 
-
-# The last complication is 0's. These basically just break the problem into a series if subproblems that still behave as described
-# above. Therefore, multiplying through from either direction is guaranteed to yield a max answer somewhere along the way. 
+# 48ms. 100 percentile. Favorite answer, although O(N) space. 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         reverse = nums[::-1]
@@ -58,3 +44,29 @@ class Solution:
             reverse[i] *= reverse[i-1] or 1
         
         return max( nums + reverse )
+
+"""
+Notes:
+
+Method 1:
+It's about the positives and negatives. Basically keep track of the biggest and smallest product up to the
+current num and then act accordingly. 
+
+
+Method 2:
+Intuition: say you have a string of numbers with a single negative in the middle. You are going to achieve
+a max sum by multiplying up to that negative on either the left side or the right side.
+Now say there are two negatives. Assuming there are positives on either side of the array, you cannot achieve a 
+max between the negatives (it will be less than 0). However, assuming you multiply through both of them, it's as 
+though they are positive. Therefore, the max will be achieved by multiplying all of the numbers.
+Say you have three negatives. This is a combination of the first two scenarios. Using all 3 in a product sticks you with
+a negative number. However, taking a product the involes the first two or the last two will yield the largest number.
+In all three of these series, if you multiply through from both the left and right side, you're guaranteed to hit
+the max product along the way.
+
+This implication holds for 4 negatives, 5, and so (you can make an inductive argument). So it always holds. 
+
+The last complication is 0's. These basically just break the problem into a series if subproblems that still behave as described
+above. Therefore, multiplying through from either direction is guaranteed to yield a max answer somewhere along the way. 
+"""
+
