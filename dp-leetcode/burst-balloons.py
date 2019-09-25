6c9beb86843bf921f565fcd1d8c86242c81d0a43
“J <jackmgrundy@gmail.com>
2019-09-25 13:54:00 -0400

    common hard

--- dp-leetcode/burst-balloons.py
+++ dp-leetcode/burst-balloons.py
@@ -14,5 +14,12 @@ Output: 167
 Explanation: nums = [3,1,5,8] --> [3,5,8] -->   [3,8]   -->  [8]  --> []
              coins =  3*1*5      +  3*5*8    +  1*3*8      + 1*8*1   = 167
 """
-# Found this to be very challenging. Could reall benefit from more DP practice.
 
+
+
+
+"""
+Notes:
+
+
+"""
