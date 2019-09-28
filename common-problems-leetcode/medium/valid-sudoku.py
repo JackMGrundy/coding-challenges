8036f2ad77456a0eda82ea9e853969cd7c35eea8
“J <jackmgrundy@gmail.com>
2019-09-27 21:10:45 -0400

    common medium

---
+++ common-problems-leetcode/medium/valid-sudoku.py
@@ -0,0 +1,76 @@
+"""
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+"""
+# 108ms. 78 percentile.
+from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows, columns, squares = defaultdict(set), defaultdict(set), defaultdict(set)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                value = board[i][j]
+                if value != ".":
+                    square = ( i//3, j//3)
+                    if value in rows[i] or value in columns[j] or value in squares[square]:
+                        return False
+                    else:
+                        rows[i].add(value)
+                        columns[j].add(value)
+                        squares[square].add(value)
+        
+        
+        return True
+
+
+"""
+Notes:
+One tricky bit in this is dealing with the squares. Easy approach is to produce a mapping from
+row and column to square. We can do that with ( i//3, j//3 )
+"""
