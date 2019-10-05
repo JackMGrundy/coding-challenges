2090b65a423cd8f75fbacfbcbccb67dc9667ee28
“J <jackmgrundy@gmail.com>
2019-10-05 14:08:40 -0400

    edits

--- companies-leetcode/facebook/dynamic-programming/decode-ways.py
+++ companies-leetcode/facebook/dynamic-programming/decode-ways.py
@@ -89,24 +89,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if s[0]=="0": return(0)    
+        if s[0] == "0": return(0)    
         last = 1
         cur = 1
 
         for i in range(1, len(s)):
             # 0's are only valid if they are paired with a preceding 1 or 2. Otherwise, the encoding
             # is invalid. 
-            if s[i]=="0" and s[i-1] not in ["1", "2"]: return(0)
+            if s[i] == "0" and s[i - 1] not in ["1", "2"]: 
+                return(0)
             # 3 cases:
             # - We hit a 0. Given previous if, the 0 must be paired with a 1 or 2. Now that
             #   1 or 2 cannot be used to generate new combinations. Reset the cur number of
             #   combinations to what it was before considering the 1 or 2.
-            if s[i]=="0":
+            if s[i] == "0":
                 last, cur = [cur, last]
             # - We hit a # and the last 2 nums are between 10 and 26 inclusive. Then
             #   f(n) = f(n-1)+f(n-2)
-            elif "10" <= s[(i-1):(i+1)] <= "26":
-                last, cur = [cur, cur+last]
+            elif "10" <= s[(i - 1):(i + 1)] <= "26":
+                last, cur = [cur, cur + last]
             # - We hit a # and the 2 nums are not between 10 and 26. Then simply set f(n) to f(n-1)
             else:
                 last, cur = [cur, cur]
