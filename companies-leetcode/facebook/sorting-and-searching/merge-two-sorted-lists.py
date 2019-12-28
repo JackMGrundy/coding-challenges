55058ba90d842ae25d295a2c51ec56348e3bb050
“J <jackmgrundy@gmail.com>
2019-12-28 14:02:13 -0500

    common problems

--- companies-leetcode/facebook/sorting-and-searching/merge-two-sorted-lists.py
+++ companies-leetcode/facebook/sorting-and-searching/merge-two-sorted-lists.py
@@ -1,5 +1,6 @@
 """
-Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+Merge two sorted linked lists and return it as a new list. The new 
+list should be made by splicing together the nodes of the first two lists.
 
 Example:
 
