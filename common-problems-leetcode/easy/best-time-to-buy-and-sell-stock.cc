fb8e4dc4668c0dfaa0a412abc7f1b43ae47119f0
“J <jackmgrundy@gmail.com>
2020-01-01 15:29:32 -0500

    common problems

--- common-problems-leetcode/easy/best-time-to-buy-and-sell-stock.cc
+++ common-problems-leetcode/easy/best-time-to-buy-and-sell-stock.cc
@@ -1,4 +1,5 @@
 /*
+
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
@@ -17,20 +18,32 @@ Example 2:
 Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
 */
-// 1st attempt: 100th percentile. ~0ms. 
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+// 4ms. 99th percentile.
+
 class Solution {
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-            return 0;
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int maxPriceSeen = 0;
+        int maxProfit = 0;
+        for (int i = prices.size() - 1; 0 <= i; i--) {
+            maxPriceSeen = std::max(maxPriceSeen, prices[i]);
+            maxProfit = std::max(maxProfit, maxPriceSeen - prices[i]);
         }
-        int res = 0;
-        int minPriceSeen = prices[0];
-        for (int i = 0; i < prices.length; i++) {
-            res = Math.max(res, prices[i]-minPriceSeen);
-            minPriceSeen = Math.min(minPriceSeen, prices[i]);
-        }
-        
-        return res;
+
+        return maxProfit;
     }
+};
+
+int main(int argc, const char* argv[]) {
+    Solution s;
+    std::vector<int> test = {7,1,5,3,6,4};
+    int result = s.maxProfit(test);
+    std::cout << "hmmmm\n";
+    std::cout << result << "\n";
 }
