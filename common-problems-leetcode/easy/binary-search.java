bae27058521ea6edb86d5346a75f164af93381c5
John Grundy <jackmgrundy@gmail.com>
2021-12-23 22:05:01 -0500

    common easy

---
+++ common-problems-leetcode/easy/binary-search.java
@@ -0,0 +1,49 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+*/
+
+// First attempt: 100th percentile
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = (left + right)/2;
+        
+        while (left <= right) {
+            int val = nums[index];
+            if (val == target) {
+                return index;
+            } else if (val < target) {
+                left = index + 1;
+            } else if (target < val) {
+                right = index - 1;
+            }
+            
+            index = (left + right)/2;
+        }
+        
+        return -1;
+    }
+}
